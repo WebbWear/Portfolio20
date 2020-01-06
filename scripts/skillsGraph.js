cb562f921a8c7df2e0ed05e1a5e1de68fec2dc5a
Jeff Webb <webbwear@icloud.com>
2020-01-06 08:42:42 -0500

    nav bar

--- scripts/skillsGraph.js
+++ scripts/skillsGraph.js
@@ -1,51 +1,57 @@
-var ctx = document.getElementById('myBarChart');
+var ctx = document.getElementById('myBarChart').getContext('2d');
 
 Chart.defaults.global.animation.duration = 4000;
 
 var myBarChart = new Chart(ctx, {
     type: 'horizontalBar',
     data: {
-        labels: ['HTML ' + '\uf13b', 'CSS ' + '\uf38b', 'JavaScript ' + '\uf3b8', 'React ' + '\uf41b', 'Ruby on Rails ' + '\uf3a5', 'Photoshop ' + '\uf778','Illustrator'],
+        labels: ['HTML ' + '\uf13b', 'CSS ' + '\uf38b', 'JavaScript ' + '\uf3b8', 'React ' + '\uf41b', 'Ruby ' + '\uf3a5', 'Photoshop', 'Illustrator'],
         datasets: [{
             label: 'My Skills',
-            data: [19, 13, 12, 10, 5, 17, 10],
+            data: [9, 7, 6, 5, 3, 7, 5],
             
 
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(241, 101, 41, 0.5)',
+                'rgba(38, 77, 228, 0.5)',
+                'rgba(240, 219, 79, 0.5)',
+                'rgba(97, 219, 251, 0.5)',
+                'rgba(213, 31, 6, 0.5)',
+                'rgba(139, 195, 252, 0.5)',
+                'rgba(251, 176, 52, 0.5)'
 
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(241, 101, 41, 1)',
+                'rgba(38, 77, 228, 1)',
+                'rgba(240, 219, 79, 1)',
+                'rgba(97, 219, 251, 1)',
+                'rgba(213, 31, 6, 1)',
+                'rgba(139, 195, 252, 1)',
+                'rgba(251, 176, 52, 1)'
 
             ],
             borderWidth: 1
         }]
     },
+
+    
+
     options: {
-        
-        
+        legend: {
+            display: false,
+        },
         scales: {
             xAxes: [{
+                ticks: {
+                    beginAtZero: true
+                },
                 gridLines: {
                     drawOnChartArea: false
                 }
             }],
             yAxes: [{
                 ticks: {
-                    beginAtZero: true,
                     fontColor: 'rgba(93, 96, 99)',
                     fontFamily: "'FontAwesome', 'Raleway'",
                     fontSize: 20
@@ -55,3 +61,31 @@ var myBarChart = new Chart(ctx, {
         
     }
 });
+
+// Below is supposed to fire when scroll into view
+// used code from example http://jsfiddle.net/TSmDV/197/
+
+// var inView = false;
+
+//     function isScrolledIntoView(elem)
+//     {
+//         var docViewTop = $(window).scrollTop();
+//         var docViewBottom = docViewTop + $(window).height();
+    
+//         var elemTop = $(elem).offset().top;
+//         var elemBottom = elemTop + $(elem).height();
+    
+//         return ((elemTop <= docViewBottom) && (elemBottom >= docViewTop));
+//     }
+    
+//     $(window).scroll(function() {
+//         if (isScrolledIntoView('#myBarChart')) {
+//             if (inView) { return; }
+//             inView = true;
+//             new Chart(
+//                 document.getElementById("myBarChart").getContext("2d"),
+//                 {type: 'horizontalBar', data: data });
+//         } else {
+//             inView = false;  
+//         }
+//     });
